8e602e2b2d592c8fbd3e436761b780c7c9bf4481
RafaelAmr93 <rafaelamaro.993@gmail.com>
2022-04-01 20:57:39 -0300

    getSingleTask e deleteTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,30 +1,55 @@
-const TaskSchema = require("../models/tasks");
+const { reset } = require("nodemon")
+const TaskSchema = require("../models/tasks")
 
-const getAllTasks = (req, res) => {
-  res.send("getAllTasks");
-};
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await TaskSchema.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
 
-const getSingleTask = (req, res) => {
-  res.json({ id: req.params.id });
-};
+const getSingleTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await TaskSchema.findOne({ _id: taskID })
+
+    if (!task) {
+      return res.status(404).json({ msg: `Id ${taskID} not found :(` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
 
 const createTask = async (req, res) => {
-  try{
-    const task = await TaskSchema.create(req.body);
-    res.status(201).json({ task });
-  } catch (error)   {
-    res.status(500).json({msg:error})
+  try {
+    const task = await TaskSchema.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
   }
-  
-};
+}
 
 const updateTask = (req, res) => {
-  res.send("updateTask");
-};
+  res.send("updateTask")
+}
 
-const deleteTask = (req, res) => {
-  res.send("deleteTask");
-};
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await TaskSchema.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `Id ${taskID} not found :(` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
 
 module.exports = {
   getAllTasks,
@@ -32,4 +57,4 @@ module.exports = {
   createTask,
   updateTask,
   deleteTask,
-};
+}
